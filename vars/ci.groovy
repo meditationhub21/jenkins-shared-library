3a90b519ecad9c2ba196b45bafefdf997dfebdf9
Shashank Nema <meditationhub21@gmail.com>
2022-06-14 17:45:12 +0530

    Updated ci.groovy file

--- vars/ci.groovy
+++ vars/ci.groovy
@@ -1,21 +1,24 @@
 #!/usr/bin/env groovy
 
-pipeline {
-    agent any
-    stages {
-        stage('build step') {
-            steps {
-                echo "Build stage is running"
+def call(String javaVersion = "java.8"){
+
+    pipeline {
+        agent any
+        stages {
+            stage('build step') {
+                steps {
+                    echo "Build stage is running"
+                }
             }
-        }
-        stage('test step') {
-            steps {
-                echo "test stage is running"
+            stage('test step') {
+                steps {
+                    echo "test stage is running"
+                }
             }
-        }
-        stage('deploy step') {
-            steps {
-                echo "deploy stage is running"
+            stage('deploy step') {
+                steps {
+                    echo "deploy stage is running"
+                }
             }
         }
     }
